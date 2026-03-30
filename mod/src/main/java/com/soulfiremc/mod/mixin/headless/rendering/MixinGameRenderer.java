94061c274389f970fc24470ed2d115d331cceeb7
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-03-30 12:05:35 +0200

    fix: block more render methods

--- mod/src/main/java/com/soulfiremc/mod/mixin/headless/rendering/MixinGameRenderer.java
+++ mod/src/main/java/com/soulfiremc/mod/mixin/headless/rendering/MixinGameRenderer.java
@@ -26,8 +26,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GameRenderer.class)
 public class MixinGameRenderer {
+  @Inject(method = "update", at = @At("HEAD"), cancellable = true)
+  private void updateHook(DeltaTracker deltaTracker, boolean advanceGameTime, CallbackInfo ci) {
+    ci.cancel();
+  }
+
+  @Inject(method = "extract", at = @At("HEAD"), cancellable = true)
+  private void extractHook(DeltaTracker deltaTracker, boolean advanceGameTime, CallbackInfo ci) {
+    ci.cancel();
+  }
+
   @Inject(method = "render", at = @At("HEAD"), cancellable = true)
-  private void renderHook(DeltaTracker deltaTracker, boolean bl, CallbackInfo ci) {
+  private void renderHook(DeltaTracker deltaTracker, boolean advanceGameTime, CallbackInfo ci) {
     ci.cancel();
   }
 
