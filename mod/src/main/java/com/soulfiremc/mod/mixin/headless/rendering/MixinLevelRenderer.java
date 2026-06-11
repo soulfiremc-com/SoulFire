f1e69b210b1094a7ad7a7344c8aa81c8ec1a244c
AlexProgrammerDE <40795980+AlexProgrammerDE@users.noreply.github.com>
2026-06-11 16:03:47 +0200

    fix(renderer): skip headless chunk ready callback

--- mod/src/main/java/com/soulfiremc/mod/mixin/headless/rendering/MixinLevelRenderer.java
+++ mod/src/main/java/com/soulfiremc/mod/mixin/headless/rendering/MixinLevelRenderer.java
@@ -19,6 +19,7 @@ package com.soulfiremc.mod.mixin.headless.rendering;
 
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.ChunkPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -39,6 +40,11 @@ public class MixinLevelRenderer {
 
   @Inject(method = "onSectionBecomingNonEmpty", at = @At("HEAD"), cancellable = true)
   private void onSectionBecomingNonEmpty(CallbackInfo ci) {
+    ci.cancel();
+  }
+
+  @Inject(method = "onChunkReadyToRender", at = @At("HEAD"), cancellable = true)
+  private void onChunkReadyToRender(ChunkPos pos, CallbackInfo ci) {
     ci.cancel();
   }
 
